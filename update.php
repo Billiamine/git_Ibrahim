77ad53131f89910783437f8a110b6bf69702f04d
Billiamine <billiamine9@gmail.com>
2022-11-17 06:38:51 -0500

    D

--- update.php
+++ update.php
@@ -1,6 +1,12 @@
 <?php
 
 require_once ('connect.php');
+
+	$id = $_GET['id'];
+	$selSql = "SELECT * FROM etudiant WHERE id=$id";
+	$res = pg_query($conn, $selSql);
+	$r = pg_fetch_assoc($res);
+
 	if (isset($_POST) & !empty($_POST)) {
 		$nom = ($_POST['nom']);
 		$prenom = ($_POST['prenom']);
@@ -8,12 +14,13 @@ require_once ('connect.php');
 		$gender = $_POST['gender'];
 		$age = $_POST['age'];
 
-		$CreateSql = "INSERT INTO etudiant (first_name,last_name,email ,gender, age)  VALUES('$nom', '$prenom', '$email', '$gender', '$age') ";
-		$res = pg_query($conn, $CreateSql) or die(pg_last_error($conn));
+		$UpdateSql = "UPDATE etudiant SET first_name='$nom',last_name='$prenom', email='$email', gender='$gender', age='$age'  WHERE id=$id ";
+
+		$res = pg_query($conn, $UpdateSql);
 		if ($res) {
-			$message = "insertion reussi avec succès";
+			header("location: view.php");
 		}else{
-			$erreur = "erreur d'insertion a la base";
+			$erreur = "la mise à jour a échoué.";
 		}
 	}
 
@@ -23,7 +30,7 @@ require_once ('connect.php');
 <!DOCTYPE html>
 <html>
 <head>
-	<title>crud app php</title>
+	<title>Formation</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="style.css" >
@@ -36,37 +43,36 @@ require_once ('connect.php');
 
 	<div class="container">
 		<div class="row pt-4">
-			<?php if (isset($message)) { ?>
-				<div class="alert alert-success" role="alert">
-					<?php echo $message; ?>
-				</div> <?php } ?>
-
 				<?php if (isset($erreur)) { ?>
 				<div class="alert alert-danger" role="alert">
 					<?php echo $erreur; ?>
 				</div> <?php } ?>
 
 			<form action="" method="POST" class="form-horizontal col-md-6 pt-4">
-				<h2>Bonjour sur Formation DEVOPS</h2>
+				<h2>Crud App by sen dev tech</h2>
 
 				<div class="form-group">
 					<label for="input1" class="col-sm-2 control-label">Nom</label>
 					<div class="col-sm-10">
-						<input type="text" name="nom" placeholder="Nom" class="form-control" id="input1">
+						<input type="text" name="nom" placeholder="Nom"
+						class="form-control" id="input1"
+						value="<?php echo $r['first_name'] ?>">
 					</div>
 				</div>
 
 				<div class="form-group">
 					<label for="input1" class="col-sm-2 control-label">prenom</label>
 					<div class="col-sm-10">
-						<input type="text" name="prenom" placeholder="prenom" class="form-control" id="input1">
+						<input type="text" name="prenom" placeholder="prenom" class="form-control" id="input1"
+						value="<?php echo $r['last_name'] ?>">
 					</div>
 				</div>
 
 				<div class="form-group">
 					<label for="input1" class="col-sm-2 control-label">Email</label>
 					<div class="col-sm-10">
-						<input type="email" name="email" placeholder="e-mail" class="form-control" id="input1">
+						<input type="email" name="email" placeholder="e-mail" class="form-control" id="input1"
+						value="<?php echo $r['email'] ?>">
 					</div>
 				</div>
 
@@ -74,12 +80,13 @@ require_once ('connect.php');
 					<label for="input1" class="col-sm-2 control-label">Genre</label>
 					<div class="col-sm-10">
 						<label>
-							<input type="radio" name="gender" id="optionsRadios" value="Homme" checked>
-							Homme
+							<input type="radio" name="gender" id="optionsRadios"
+							value="h" <?php if($r['gender'] == 'h'){ echo "checked";} ?>>
+							H
 						</label>
 						<label>
-							<input type="radio" name="gender" id="optionsRadios" value="Femme" checked>
-							Femme
+							<input type="radio" name="gender" id="optionsRadios" value="f" <?php if($r['gender'] == 'f'){ echo "checked";} ?>>
+							F
 						</label>
 
 					</div>
@@ -90,11 +97,15 @@ require_once ('connect.php');
 					<div class="col-sm-10">
 						<select name="age" class="form-control">
 							<option>Ton age</option>
-							<option value="20">20</option>
-							<option value="21">21</option>
-							<option value="22">22</option>
-							<option value="23">23</option>
-							<option value="24">24</option>
+							<option value="20" <?php if($r['age'] == '20'){ echo "selected";} ?>>20</option>
+
+							<option value="21"  <?php if($r['age'] == '21'){ echo "selected";} ?>>21</option>
+
+							<option value="22"  <?php if($r['age'] == '22'){ echo "selected";} ?> >22</option>
+
+							<option value="23" <?php if($r['age'] == '23'){ echo "selected";} ?> >23</option>
+
+							<option value="24" <?php if($r['age'] == '24'){ echo "selected";} ?>>24</option>
 
 						</select>
 					</div>
@@ -102,11 +113,7 @@ require_once ('connect.php');
 
 				<div class="pt-4">
 					<input type="submit" value="submit" class="btn btn-primary m-3">
-					<a href="view.php">
-						<button class="btn btn-success m-3" type="button">
-							voir la liste
-						</button>
-					</a>
+
 				</div>
 			</form>
 		</div>
